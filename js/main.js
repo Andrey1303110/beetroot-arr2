10c01062ad9d1d9c7f36f09d3871df253f314f06
Andrey1303110 <andrey1303110@gmail.com>
2020-12-02 19:56:09 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 function printArray(a) {
     document.write("[ ");
     for (let i = 0; i < a.length; i++) {
-        document.write(`${a[i]}`);
+        document.write(`${objToString(a[i])}`);
 
         if (i != a.length - 1) {
             document.write(", ");
         }
     }
     document.write(" ]<br />");
+}
+
+function objToString(obj) {
+    if (typeof obj == "object") {
+        return `${obj.name}, ${obj.gender}, (${obj.age})`
+    }
+    return obj;
 }
 
 var arr = [1, 5, 7, 2, 0, -1, 8];
@@ -32,4 +39,33 @@ document.write(el1 + "<br />");
 
 arr.sort();
 printArray(arr);
+
+arr.sort((a, b) => {
+    return a - b;
+});
+printArray(arr);
+
+var people = [
+    {
+        name: "Anna",
+        gender: "female",
+        age: 21
+    },
+    {
+        name: "Grisha",
+        gender: "male",
+        age: 45
+    },
+    {
+        name: "Sanya",
+        gender: "male",
+        age: 10
+    },
+    {
+        name: "Olga",
+        gender: "female",
+        age: 60
+    }
+];
+
 
