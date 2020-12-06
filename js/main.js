b2ebc857eb933de791eb2cfbf9a1ca5b89c10e62
Andrey1303110 <andrey1303110@gmail.com>
2020-12-06 13:01:00 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -4,7 +4,11 @@ function printArray(a) {
         document.write(`${objToString(a[i])}`);
 
         if (i != a.length - 1) {
-            document.write(", ");
+            if (typeof a[i] == "object") {
+                document.write("<br />");
+            } else {
+                document.write(", ");
+            }
         }
     }
     document.write(" ]<br />");
@@ -12,38 +16,38 @@ function printArray(a) {
 
 function objToString(obj) {
     if (typeof obj == "object") {
-        return `${obj.name}, ${obj.gender}, (${obj.age})`
+        let objStr = "";
+        for (let prop in obj) {
+            objStr += `, ${prop}: ${obj[prop]}`;
+        }
+
+        return objStr.substr(2);
     }
+
     return obj;
 }
 
-var arr = [1, 5, 7, 2, 0, -1, 8];
+/*var arr = [1, 5, 7, 2, 0, -1, 8];
 printArray(arr);
-
 arr.push(13);
 printArray(arr);
-
-arr.push(17, 12, 42);
+var l = arr.push(17, 12, 42);
 printArray(arr);
-
+document.write(l + "<br />");
 var el = arr.pop();
 printArray(arr);
 document.write(el + "<br />");
-
 arr.unshift(1, 2, 3);
 printArray(arr);
-
 var el1 = arr.shift();
 printArray(arr);
 document.write(el1 + "<br />");
-
 arr.sort();
 printArray(arr);
-
 arr.sort((a, b) => {
-    return a - b;
+  return a - b;
 });
-printArray(arr);
+printArray(arr);*/
 
 var people = [
     {
@@ -52,20 +56,87 @@ var people = [
         age: 21
     },
     {
-        name: "Grisha",
+        name: "Boris",
         gender: "male",
         age: 45
     },
     {
-        name: "Sanya",
+        name: "Misha",
         gender: "male",
-        age: 10
+        age: 45
     },
     {
         name: "Olga",
         gender: "female",
-        age: 60
-    }
+        age: 45
+    },
+    {
+        name: "Elena",
+        gender: "female",
+        age: 19
+    },
 ];
+printArray(people);
 
+people.sort((el1, el2) => {
+    var ageComp = el1.age - el2.age;
+    if (ageComp != 0) {
+        return ageComp;
+    }
 
+    return -el1.name.localeCompare(el2.name);
+});
+printArray(people);
+
+var peopleWithSkinColor = people.map((el) => {
+    if (el.gender == "female") {
+        el.skin = "black";
+    } else {
+        el.skin = "orange";
+    }
+
+    return el;
+});
+printArray(peopleWithSkinColor);
+
+var filteredPeople = peopleWithSkinColor.filter((human) => {
+    return !(["orange", "white", "blue"].includes(human.skin));
+});
+printArray(filteredPeople);
+
+/*let numbers = [5, 3, 1, 4, 2];
+let squares = numbers.map((a) => {
+  return a**2; // a*a
+}).sort((a, b) => {
+  return a - b;
+});
+printArray(numbers);
+printArray(squares);*/
+
+let numbers = [5, 3, 1, 4, 2];
+printArray(numbers);
+//printArray(numbers.filter(a => a%2 ));
+
+//document.write(Array.isArray(numbers));
+//document.write(Array.isArray(123));
+
+document.write(
+    numbers.reduce(
+        (sum, el, index/*, arr*/) => {
+            if (index % 2 == 0) {
+                return sum + el;
+            }
+
+            return sum;
+        },
+        0
+    )
+);
+
+var obj = [
+    "Elena",
+    "female",
+    19
+];
+console.log(obj);
+console.log(obj.values());
